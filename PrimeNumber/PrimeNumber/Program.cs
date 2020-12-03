c357cb077f167ec0fb7af9f53849fa6d67e0ca25
Your Name <you@example.com>
2020-12-03 10:47:23 +0300

    primenumber

---
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (IsPrimeNumber(7))
+            {
+                Console.WriteLine("This is a Prime Number");
+            }
+            else
+            {
+                Console.WriteLine("This is not a Prime Number");
+            }
+            Console.ReadLine();
+        }
+
+        private static bool IsPrimeNumber(int number)
+        {
+            bool result = true;
+            for (int i = 2; i < number - 1; i++)
+            {
+                if (number % i == 0)
+                {
+                    result = false;
+                    i = number;
+                }
+            }
+            return result;
+        }
+    }
+    }
+}
