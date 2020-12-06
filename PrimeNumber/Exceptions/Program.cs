a725990aa8937b4904003f8584dd753555bbd719
Your Name <you@example.com>
2020-12-07 02:40:40 +0300

    Exceptions

---
+++ PrimeNumber/Exceptions/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string[] students = new string[3] { "Ezgi", "Ece", "Orkun" };
+                students[3] = "Büşra"; //Arrayler 0'dan başladığı için üst sınırı 2 olan bir array'i olmayan bir üç ile değiştirmeye çalışmak hata verir
+
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.InnerException); //Daha detaylı hatayı açıklar
+
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
