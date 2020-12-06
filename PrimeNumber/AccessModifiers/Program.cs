c0a625992513ef69a870930781f3311b341116a3
Your Name <you@example.com>
2020-12-07 00:01:21 +0300

    AccessModifiers

---
+++ PrimeNumber/AccessModifiers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Customer
+    {
+        private int Id;
+        public void Save()
+        {
+
+        }
+        public void Delete()
+        {
+
+        }
+    }
+    class Student : Customer
+    {
+        public void Save2()
+        {
+            Customer customer = new Customer();
+            
+        }
+    }
+
+    class Course //bir class'ın başında public, private ya da protected yazmıyorsa bu class default internal'dir. 
+    {
+        public string Name { get; set; }
+    }
+}
