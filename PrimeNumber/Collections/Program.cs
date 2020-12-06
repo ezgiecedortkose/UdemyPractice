042b2ff088b94eafbdf2ca72b6de7348434eb82b
Your Name <you@example.com>
2020-12-07 01:33:37 +0300

    Collections

---
+++ PrimeNumber/Collections/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cities = new string[2] { "Ankara", "İstanbul" };
+
+            //new string diyerek yeni bir array oluşturduk.
+            //Bu nedenle yukardakilerin bir hükmü kalmadı.
+            //Yeni oluşturulan bu 3 array'e de bir atama yapılmadığı için içi şu an boş
+            //Ekrana yazdırmak istediğimizde boş bir ekran görmemizin nedeni bu
+            cities = new string[3];
+
+            Console.WriteLine(cities[0]);
+
+            Console.ReadLine();
+        }
+    }
+}
