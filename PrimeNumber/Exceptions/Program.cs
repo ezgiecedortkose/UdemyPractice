8870a8ba50777ee19b470ee0e19570c2284a7d60
Your Name <you@example.com>
2020-12-07 02:51:34 +0300

    Exception Method

--- PrimeNumber/Exceptions/Program.cs
+++ PrimeNumber/Exceptions/Program.cs
@@ -12,8 +12,39 @@ namespace Exceptions
         {
             try
             {
-                string[] students = new string[3] { "Ezgi", "Ece", "Orkun" };
-                students[3] = "Büşra"; //Arrayler 0'dan başladığı için üst sınırı 2 olan bir array'i olmayan bir üç ile değiştirmeye çalışmak hata verir
+                List<string> students = new List<string> { "Ezgi", "Ece", "Orkun" };
+                if (!students.Contains("Büşra"))
+                {
+                    throw new RecordNotFoundException();
+                }
+                else
+                {
+                    Console.WriteLine("Record Found");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+
+            //List<string> students = new List<string> { "Ezgi", "Ece", "Orkun" };
+            //if (!students.Contains("Büşra"))
+            //{
+            //    throw new RecordNotFoundException();
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Record Found");
+            //}
+
+
+
+            try
+            {
+                string[] student = new string[3] { "Ezgi", "Ece", "Orkun" };
+                //student[3] = "Büşra"; //Arrayler 0'dan başladığı için üst sınırı 2 olan bir array'i olmayan bir üç ile değiştirmeye çalışmak hata verir
 
 
             }
@@ -24,9 +55,6 @@ namespace Exceptions
                 Console.WriteLine(ex.InnerException); //Daha detaylı hatayı açıklar
 
             }
-
-
-
 
             Console.ReadLine();
         }
