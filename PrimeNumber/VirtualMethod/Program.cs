86d5e96e383408a22451b81de5d204f682cc4447
Your Name <you@example.com>
2020-12-06 23:05:54 +0300

    Virtual Method

---
+++ PrimeNumber/VirtualMethod/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlServer sqlServer = new SqlServer();
+            sqlServer.Add();
+            MySql mySql = new MySql();
+            mySql.Add();
+
+            Console.ReadLine();
+        }
+    }
+
+    class Database
+    {
+        public virtual void Add()
+        {
+            Console.WriteLine("Added by Default");
+        }
+
+        public virtual void Delete()
+        {
+            Console.WriteLine("Deleted by Default");
+        }
+    }
+
+    class SqlServer : Database
+    {
+        public override void Add()
+        {
+            Console.WriteLine("Added by SqlServer");
+           // base.Add();
+        }
+    }
+
+    class MySql : Database
+    {
+
+    }
+}
