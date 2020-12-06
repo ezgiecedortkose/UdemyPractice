8c74e91858570a2937363ac530c7a4bed5e66471
Your Name <you@example.com>
2020-12-06 16:18:44 +0300

    strings

---
+++ PrimeNumber/Strings/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string city = "İstanbul";
+            string city2 = "İzmir";
+
+            foreach (var item in city)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(String.Format("{0} {1}", city, city2));
+
+            Console.ReadLine();
+        }
+    }
+}
