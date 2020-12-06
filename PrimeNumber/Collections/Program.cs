05810f51ec4c3beeb179a2e4c57b1da8960feb60
Your Name <you@example.com>
2020-12-07 01:38:58 +0300

    ArrayList

--- PrimeNumber/Collections/Program.cs
+++ PrimeNumber/Collections/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,15 +11,30 @@ namespace Collections
     {
         static void Main(string[] args)
         {
-            string[] cities = new string[2] { "Ankara", "İstanbul" };
+            string[] cities2 = new string[2] { "Ankara", "İstanbul" };
 
             //new string diyerek yeni bir array oluşturduk.
             //Bu nedenle yukardakilerin bir hükmü kalmadı.
             //Yeni oluşturulan bu 3 array'e de bir atama yapılmadığı için içi şu an boş
             //Ekrana yazdırmak istediğimizde boş bir ekran görmemizin nedeni bu
-            cities = new string[3];
+            cities2 = new string[3];
 
-            Console.WriteLine(cities[0]);
+            Console.WriteLine(cities2[0]);
+
+
+
+            ArrayList cities = new ArrayList();
+            cities.Add("Ankara");
+            cities.Add("Adana");
+            cities.Add(1);
+
+            foreach (var city in cities)
+            {
+                Console.WriteLine(city);
+            }
+
+            cities.Add("İstanbul");
+            Console.WriteLine(cities[3]);
 
             Console.ReadLine();
         }
