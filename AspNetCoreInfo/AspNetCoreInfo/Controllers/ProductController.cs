9e5cac8a9e0cfdceefac928722b7e13bcdd8f3b2
Your Name <you@example.com>
2020-12-12 23:10:58 +0300

    asp.netcore httppost

--- AspNetCoreInfo/AspNetCoreInfo/Controllers/ProductController.cs
+++ AspNetCoreInfo/AspNetCoreInfo/Controllers/ProductController.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetCoreInfo.Entities;
+using AspNetCoreInfo.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AspNetCoreInfo.Controllers
@@ -12,5 +14,25 @@ namespace AspNetCoreInfo.Controllers
         {
             return View();
         }
+
+        public IActionResult Index3()
+        {
+            List<Customer> customers = new List<Customer>()
+            {
+                new Customer{Id=1, FirstName="Ezgi", LastName="Dörtköşe", City="İstanbul"},
+                new Customer{Id=2, FirstName="Ece",LastName="Dörtköşe", City="Ankara"},
+                new Customer{Id=3, FirstName="Orkun",LastName="Dörtköşe", City="İzmir"}
+            };
+            List<string> shops = new List<string> { "Ankara", "İstanbul", "İzmir" };
+
+            var model = new CustomerListViewModel
+            {
+                Customers = customers,
+                Shops = shops
+            };
+            return View(model);
+        }
+           
     }
 }
+
